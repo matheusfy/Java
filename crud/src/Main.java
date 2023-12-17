19ea4e16c853b5d89947c8da75d6a7ef5b0cd144
matheusfy <matheusy@alunos.utfpr.edu.br>
2023-12-17 15:42:03 -0300

    Commit crud

--- crud/src/Main.java
+++ crud/src/Main.java
@@ -14,14 +14,13 @@ public class Main {
     public static void main(String[] args) {
 
         String table_name = "contatos";
-        Crud crud         = new Crud();
-        Utils utility     = new Utils();
-        List<Contato> Contatos;
+        Crud crud = new Crud();
+        Connection connection_db;
 
+        // Teste do banco
         try {
-            Connection connection_db  = crud.connect();
+            connection_db = crud.connect();
 
-            /**** Criando tabela do banco *********/
             try {
                 // # Cria tabela
                 crud.CreateTable(connection_db, "contatos");
@@ -29,37 +28,41 @@ public class Main {
                 throw new RuntimeException(e);
             }
 
-            /**** Adicionando contatos da tabela do banco *********/
-            try {
-                // # Adiciona linhas na tabela
-                String registro = utility.reg_contato(table_name,"matheus", 27, "25-Jul-2023");
-                crud.Add(connection_db, registro);
+            TesteBanco(connection_db, table_name, crud);
 
-            } catch(Exception e) {
-                System.out.println("Erro ao tentar salvar no banco. "+ e);
-            }
-
-            /**** Pegando lista de contatos da tabela do banco *********/
-            try{
-                DateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy");
-                String strDate;
-                Contatos = crud.GetList(connection_db, table_name);
-
-                for (Contato contato: Contatos){
-                    strDate = dateFormat.format(contato.getData_cadastro());
-                    System.out.println(contato.getNome() + " " + Integer.toString(contato.getIdade()) + " " + strDate);
-                }
-
-            } catch (Exception e) {
-
-            }
-        } catch (Exception e) {
-          System.out.println("Connection fail. " + e);
+            System.out.println("Teste finalizando");
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
         }
 
-
-
-        System.out.println(" finalizando ");
     }
 
+    private static void TesteBanco(Connection connection_db, String table_name, Crud crud) {
+
+        Utils utility = new Utils();
+        List<Contato> Contatos;
+
+        try {
+            // # Adiciona linhas na tabela
+            String registro = utility.reg_contato(table_name, "matheus", 27, "25-Jul-2023");
+            crud.Add(connection_db, registro);
+
+        } catch (Exception e) {
+            System.out.println("Erro ao tentar salvar no banco. " + e);
+        }
+
+        try {
+            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+            String strDate;
+            Contatos = crud.GetList(connection_db, table_name);
+
+            for (Contato contato : Contatos) {
+                strDate = dateFormat.format(Contato.getData_cadastro());
+                System.out.println(Contato.getNome() + " " + Contato.getIdade() + " " + strDate);
+            }
+
+        } catch (Exception e) {
+
+        }
+    }
 }
